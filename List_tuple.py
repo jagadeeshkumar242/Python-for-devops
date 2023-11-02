e4e46d82bebe445ef7228f3eebabe74a2a08910c
jagadeeshkumar242 <48792659+jagadeeshkumar242@users.noreply.github.com>
2023-11-02 22:27:40 +0530

    Create List_tuple.py

---
+++ List_tuple.py
@@ -0,0 +1,13 @@
+#lists
+
+students_names = ["Jagadeesh", "mohan", "Raja"]
+# students_names.append("RamKrishna")
+# students_names.remove("Raja")
+# print(students_names)
+print(len(students_names))
+# for i in students_names:
+#     print(i)
+
+#tuple
+# students_names = ("Raja", "Rani", "Bantu")
+# print(type(students_names))
