aa0460c2ec4ef56a3e9b29824372f2dcb1ce35d9
jagadeeshkumar242 <48792659+jagadeeshkumar242@users.noreply.github.com>
2023-11-03 18:46:21 +0530

    Update List_tuple.py

--- List_tuple.py
+++ List_tuple.py
@@ -4,10 +4,26 @@ students_names = ["Jagadeesh", "mohan", "Raja"]
 # students_names.append("RamKrishna")
 # students_names.remove("Raja")
 # print(students_names)
-print(len(students_names))
+# print(len(students_names))
 # for i in students_names:
 #     print(i)
 
 #tuple
 # students_names = ("Raja", "Rani", "Bantu")
 # print(type(students_names))
+
+# slicing
+
+# s3_bucket_list = ["s3_bucket1", "s3_bucket2", "s3_bucket3", "s3_bucket4"]
+# print(s3_bucket_list[0:2])
+
+# numbers = [1, 20, 15]
+# numbers.sort()
+# print(numbers)
+
+# string concatning
+# string1 = ["Ramesh", "Rakesh", "Rajesh"]
+# print(string1[0] + " " + string1[2])
+
+# Random_string = [ 1, 2, 3, "Rajesh", "suresh", 2.3, 4.67]
+# print(Random_string)
