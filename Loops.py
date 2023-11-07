dea4738d9f1f65cac6630f791d3c2ec62b7d21a2
jagadeeshkumar242 <48792659+jagadeeshkumar242@users.noreply.github.com>
2023-11-07 23:44:03 +0530

    Create Loops.py

---
+++ Loops.py
@@ -0,0 +1,24 @@
+for i in range(5):
+    print("Abhishek")
+    print("Veermalla")
+for i in range(10):
+    print(i)
+for i in range(10):
+    print(i)
+    print("-Jagadeesh")
+
+fruits = ["Apple", "Banana", "Cherry"]
+for fruit in fruits:
+    print(fruit)
+
+numbers = [1,2,3,4,5]
+for i in numbers:
+    if i == 3:
+     break
+    print(i)
+
+Numbers = [1,2,3,4,5]
+for number in Numbers:
+    if number == 3:
+        continue
+    print(number)
